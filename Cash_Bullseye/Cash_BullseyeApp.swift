b6fa69d6521fa62914436fd972c5c18b3ec1d367
Cash <rockwellcash@gmail.com>
2022-01-19 07:04:39 -0500

    Initial Commit

---
+++ Cash_Bullseye/Cash_BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cash_BullseyeApp.swift
+//  Cash_Bullseye
+//
+//  Created by Cash on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Cash_BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cash_Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cash_Bullseye
+//
+//  Created by Cash on 1/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
